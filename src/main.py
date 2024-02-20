d56b24a98923ad7daf5cff1f1d21ac903260f318
elianderlohr <elias.anderlohr@t-online.de>
2024-02-20 13:32:08 +0100

    Ignore warnings related to empty slices and invalid values in double scalars

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ import warnings
 warnings.filterwarnings(
     "ignore", message="Mean of empty slice.", category=RuntimeWarning
 )
+warnings.filterwarnings("ignore", message="invalid value encountered in double_scalars")
 
 
 # welcome console message
