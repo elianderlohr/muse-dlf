0e659ae1079fecd54f72f8f60d48da9460f6bac7
elianderlohr <elias.anderlohr@t-online.de>
2024-02-25 20:04:56 +0100

    Update W&B API key in mlm.sh script

--- run/mlm.sh
+++ run/mlm.sh
@@ -9,18 +9,11 @@
 # Install necessary packages
 pip install datasets wandb
 
-# Check if W&B API key is provided as a command-line argument
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 WANDB_API_KEY"
-    exit 1
-fi
-
-
 DATA_PATH="../data/mfc/"
 OUTPUT_PATH="../models/roberta-base-finetune/"
 
 # Set the W&B API key as an environment variable
-WANDB_API_KEY=$1
+WANDB_API_KEY="XXXX"
 
 # Run the Python script with the W&B API key
 python train_model.py --wb_api_key $WANDB_API_KEY --data_path $DATA_PATH --output_path $OUTPUT_PATH
