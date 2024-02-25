82d83c615d31ac2677faf4a3163c4022a0590287
elianderlohr <elias.anderlohr@t-online.de>
2024-02-25 20:19:13 +0100

    Add environment variable loading from .env file and check for W&B API key

--- run/mlm.sh
+++ run/mlm.sh
@@ -6,14 +6,27 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elias.anderlohr@gmail.com
 
+# Load the environment variables from the .env file
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found"
+    exit 1
+fi
+
 # Install necessary packages
-pip install datasets wandb
+pip install datasets wandb transformers
 
 DATA_PATH="../data/mfc/"
 OUTPUT_PATH="../models/roberta-base-finetune/"
 
 # Set the W&B API key as an environment variable
-WANDB_API_KEY="XXXX"
+# Check if W&B API key is set
+if [ -z "${WANDB_API_KEY}" ]; then
+    echo "WANDB_API_KEY is not set. Please check your .env file."
+    exit 1
+fi
+
 
 # Run the Python script with the W&B API key
 python ../src/training/mlm.py --wb_api_key $WANDB_API_KEY --data_path $DATA_PATH --output_path $OUTPUT_PATH
