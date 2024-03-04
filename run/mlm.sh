10dabdc387041f81d9acb78ce34a356d530a8bbb
elianderlohr <elias.anderlohr@t-online.de>
2024-03-04 16:09:35 +0100

    Update mlm.sh script to use Python 3.9 and accelerate config

--- run/mlm.sh
+++ run/mlm.sh
@@ -7,52 +7,31 @@
 #SBATCH --mail-user=elias.anderlohr@gmail.com
 #SBATCH --gres=gpu:2
 
-# Load Python 3.9 module if necessary
-# Uncomment the next line if you're on a cluster that uses modules to manage software
-# module load python/3.9
-
-# Load the environment variables from the .env file
-if [ -f .env ]; then
-    export $(cat .env | xargs)
-else
-    echo ".env file not found"
-    exit 1
-fi
-
-# Check if W&B API key is set
-if [ -z "${WANDB_API_KEY}" ]; then
-    echo "WANDB_API_KEY is not set. Please check your .env file."
-    exit 1
-fi
-
-# list all available python versions
-ls -ls /usr/bin/python*
-
-# Create a virtual environment specifically with Python 3.9
+# Create a virtual environment with Python 3.9
 echo "Creating virtual environment with Python 3.9..."
 python3.9 -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
 
-# print available python version
-python3.9 --version
+# Verify Python version
+python --version
 
-# Upgrade pip and Install necessary packages within the virtual environment
+# Upgrade pip and install necessary packages within the virtual environment
 echo "Installing necessary packages..."
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r run/requirements.txt
+pip install --upgrade pip
+pip install -r run/requirements.txt
 
-# echo versions of installed packages
+# List installed packages for verification
 echo "Installed packages:"
-python3.9 -m pip list
+pip list
 
 DATA_PATH="data/mfc/"
 
-# create output path with timestamp subdir
+# Create output path with timestamp subdir
 OUTPUT_PATH="models/roberta-base-finetune/$(date +'%Y-%m-%d_%H-%M-%S')/"
 
-# Print the output of nvidia-smi
+# Print GPU status
 echo "GPU status:"
 nvidia-smi
 
@@ -63,13 +42,13 @@ echo "Starting training script..."
 
 echo "Set up accelerate config using default"
 
-# accelerate config
-python3.9 -m accelerate config default --mixed_precision fp16
+# Use accelerate config with Python 3.9
+accelerate config
 
 echo "Start training script with accelerate launch"
 
-# launch accelerates training script with multi_gpu and num_processes
-python3.9 -m accelerate launch --multi_gpu --num_processes 2 --num_machines 2 --mixed_precision fp16 src/training/mlm.py --wb_api_key $WANDB_API_KEY --data_path $DATA_PATH --output_path $OUTPUT_PATH --batch_size 32 --epochs 10
+# Ensure accelerate uses Python 3.9
+accelerate launch --multi_gpu --num_processes 2 --num_machines 1 --mixed_precision fp16 src/training/mlm.py --wb_api_key $WANDB_API_KEY --data_path $DATA_PATH --output_path $OUTPUT_PATH --batch_size 32 --epochs 10
 
 # Deactivate the virtual environment
 deactivate
