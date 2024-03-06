bbd7667d615b3215103e0e84cd8ad64d53fd7d0a
elianderlohr <elias.anderlohr@t-online.de>
2024-03-06 13:36:01 +0100

    Update time limit for SLURM job

--- run/mlm.sh
+++ run/mlm.sh
@@ -3,7 +3,7 @@
 # SLURM Directives
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=12:00:00
+#SBATCH --time=00:01:00
 #SBATCH --job-name=roberta-base-finetune
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elias.anderlohr@gmail.com
