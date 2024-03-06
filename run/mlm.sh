ed107f5ebd3aaea7edc8df41c2bb1bc4b49510a6
elianderlohr <elias.anderlohr@t-online.de>
2024-03-06 09:49:47 +0100

    Update SLURM directives and enable dataloader_num_workers

--- run/mlm.sh
+++ run/mlm.sh
@@ -2,8 +2,8 @@
 
 # SLURM Directives
 #SBATCH --ntasks=1
-#SBATCH --nodes=2
-#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
 #SBATCH --job-name=roberta-base-finetune
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elias.anderlohr@gmail.com
