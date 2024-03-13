45dc0b1fe694c512a31c8c69e5d23d943018db5f
elianderlohr <elias.anderlohr@t-online.de>
2024-03-13 08:57:04 +0100

    Add evaluate metrics for evaluation

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -7,7 +7,7 @@ import json
 from tqdm import tqdm
 from datetime import datetime
 import math
-from accelerate import Accelerator
+import evaluate
 
 
 class Trainer:
@@ -37,6 +37,7 @@ class Trainer:
             scheduler: The learning rate scheduler to be used for training.
             device: The device to be used for training.
             save_path: The path to save the model and metrics.
+            accelerator: The accelerator to be used for training.
             tau_min: The minimum value of tau.
             tau_decay: The decay factor for tau.
 
@@ -171,6 +172,11 @@ class Trainer:
         combined_preds = []
         all_labels = []
 
+        # Load the evaluate metrics
+        f1_metric_micro = evaluate.load("f1", config_name="micro")
+        f1_metric_macro = evaluate.load("f1", config_name="macro")
+        accuracy_metric = evaluate.load("accuracy")
+
         for batch_idx, batch in enumerate(
             tqdm(test_dataloader, desc=f"Evaluate - Epoch {epoch}")
         ):
@@ -204,6 +210,10 @@ class Trainer:
                     tau,
                 )
 
+            span_pred = (torch.softmax(span_logits, dim=1) > 0.5).int()
+            sentence_pred = (torch.softmax(sentence_logits, dim=1) > 0.5).int()
+            combined_pred = (torch.softmax(combined_logits, dim=1) > 0.5).int()
+
             span_logits = self.accelerator.gather_for_metrics((span_logits, labels))
             sentence_logits = self.accelerator.gather_for_metrics(
                 (sentence_logits, labels)
@@ -211,10 +221,6 @@ class Trainer:
             combined_logits = self.accelerator.gather_for_metrics(
                 (combined_logits, labels)
             )
-
-            span_pred = (torch.softmax(span_logits, dim=1) > 0.5).int()
-            sentence_pred = (torch.softmax(sentence_logits, dim=1) > 0.5).int()
-            combined_pred = (torch.softmax(combined_logits, dim=1) > 0.5).int()
 
             span_preds.append(span_pred.cpu().numpy())
             sentence_preds.append(sentence_pred.cpu().numpy())
@@ -240,46 +246,65 @@ class Trainer:
         all_combined_preds = np.vstack(combined_preds)
         all_labels = np.vstack(all_labels)
 
-        # Compute metrics
-        f1_span_micro = f1_score(
-            all_labels, all_span_preds, average="micro", zero_division=0
+        # Calculate metrics for span predictions
+        span_results_micro = f1_metric_micro.compute(
+            predictions=all_span_preds.tolist(), references=all_labels.tolist()
         )
-        f1_sentence_micro = f1_score(
-            all_labels, all_sentence_preds, average="micro", zero_division=0
+        span_results_macro = f1_metric_macro.compute(
+            predictions=all_span_preds.tolist(), references=all_labels.tolist()
         )
-        f1_combined_micro = f1_score(
-            all_labels, all_combined_preds, average="micro", zero_division=0
-        )
-
-        f1_span_macro = f1_score(
-            all_labels, all_span_preds, average="macro", zero_division=0
-        )
-        f1_sentence_macro = f1_score(
-            all_labels, all_sentence_preds, average="macro", zero_division=0
-        )
-        f1_combined_macro = f1_score(
-            all_labels, all_combined_preds, average="macro", zero_division=0
+        span_accuracy = accuracy_metric.compute(
+            predictions=all_span_preds.tolist(), references=all_labels.tolist()
         )
 
-        accuracy_span = accuracy_score(all_labels, all_span_preds)
-        accuracy_sentence = accuracy_score(all_labels, all_sentence_preds)
-        accuracy_combined = accuracy_score(all_labels, all_combined_preds)
+        # Calculate metrics for sentence predictions
+        sentence_results_micro = f1_metric_micro.compute(
+            predictions=all_sentence_preds.tolist(), references=all_labels.tolist()
+        )
+        sentence_results_macro = f1_metric_macro.compute(
+            predictions=all_sentence_preds.tolist(), references=all_labels.tolist()
+        )
+        sentence_accuracy = accuracy_metric.compute(
+            predictions=all_sentence_preds.tolist(), references=all_labels.tolist()
+        )
 
-        print(
-            f"Validation Metrics - micro F1 - Span: {f1_span_micro:.2f}, Sentence: {f1_sentence_micro:.2f}, Combined: {f1_combined_micro:.2f}, macro F1 - Span: {f1_span_macro:.2f}, Sentence: {f1_sentence_macro:.2f}, Combined: {f1_combined_macro:.2f}"
+        # Calculate metrics for combined predictions
+        combined_results_micro = f1_metric_micro.compute(
+            predictions=all_combined_preds.tolist(), references=all_labels.tolist()
+        )
+        combined_results_macro = f1_metric_macro.compute(
+            predictions=all_combined_preds.tolist(), references=all_labels.tolist()
+        )
+        combined_accuracy = accuracy_metric.compute(
+            predictions=all_combined_preds.tolist(), references=all_labels.tolist()
+        )
+
+        self.accelerator.print("Span Metrics:")
+        self.accelerator.print(
+            f"F1 Micro: {span_results_micro['f1']:.4f}, F1 Macro: {span_results_macro['f1']:.4f}, Accuracy: {span_accuracy['accuracy']:.4f}"
+        )
+
+        self.accelerator.print("Sentence Metrics:")
+        self.accelerator.print(
+            f"F1 Micro: {sentence_results_micro['f1']:.4f}, F1 Macro: {sentence_results_macro['f1']:.4f}, Accuracy: {sentence_accuracy['accuracy']:.4f}"
+        )
+
+        self.accelerator.print("Combined Metrics:")
+        self.accelerator.print(
+            f"F1 Micro: {combined_results_micro['f1']:.4f}, F1 Macro: {combined_results_macro['f1']:.4f}, Accuracy: {combined_accuracy['accuracy']:.4f}"
         )
 
         metrics = {
+            "span_results_micro": span_results_micro,
+            "span_results_macro": span_results_macro,
+            "span_accuracy": span_accuracy,
+            "sentence_results_micro": sentence_results_micro,
+            "sentence_results_macro": sentence_results_macro,
+            "sentence_accuracy": sentence_accuracy,
+            "combined_results_micro": combined_results_micro,
+            "combined_results_macro": combined_results_macro,
+            "combined_accuracy": combined_accuracy,
             "epoch": epoch,
-            "accuracy_span": accuracy_span,
-            "accuracy_sentence": accuracy_sentence,
-            "accuracy_combined": accuracy_combined,
-            "f1_span_micro": f1_span_micro,
-            "f1_sentence_micro": f1_sentence_micro,
-            "f1_combined_micro": f1_combined_micro,
-            "f1_span_macro": f1_span_macro,
-            "f1_sentence_macro": f1_sentence_macro,
-            "f1_combined_macro": f1_combined_macro,
         }
 
         self.accelerator.log(metrics)
