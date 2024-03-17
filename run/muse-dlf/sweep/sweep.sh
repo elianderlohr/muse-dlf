793f10b98b20ab4b0d75320cfb9b19ea0f3069d0
elianderlohr <elias.anderlohr@t-online.de>
2024-03-17 14:38:57 +0100

    Update GPU resource allocation in sweep.sh

--- run/muse-dlf/sweep/sweep.sh
+++ run/muse-dlf/sweep/sweep.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=roberta-base-finetune
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elias.anderlohr@gmail.com
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 
 echo "===================== Job Details ====================="
 # Activate the virtual environment
