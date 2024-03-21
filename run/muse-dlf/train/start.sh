74df771d9b8266a2aac6a4a465d2bf91e46f1315
elianderlohr <elias.anderlohr@t-online.de>
2024-03-21 09:17:52 +0100

    Add accelerate_config.yaml and start.sh scripts for training

--- run/muse-dlf/train/start.sh
+++ run/muse-dlf/train/start.sh
@@ -62,7 +62,7 @@ export TORCH_DISTRIBUTED_DEBUG=DETAIL
 echo "=================== Training Start ==================="
 
 echo "Launching training script with Accelerate..."
-accelerate launch --multi_gpu --num_processes 2 --num_machines 1 --mixed_precision fp16 --config_file run/muse-dlf/accelerate_config.yaml src/train.py \
+accelerate launch --multi_gpu --num_processes 2 --num_machines 1 --mixed_precision fp16 --config_file run/muse-dlf/train/accelerate_config.yaml src/train.py \
     --wandb_api_key $WANDB_API_KEY \
     --path_data $DATA_PATH \
     --batch_size 32 \
@@ -74,7 +74,14 @@ accelerate launch --multi_gpu --num_processes 2 --num_machines 1 --mixed_precisi
     --path_frameaxis data/frameaxis/mfc/frameaxis_mft.pkl \
     --path_antonym_pairs data/axis/mft.json \
     --dim_names virtue,vice \
-    --save_path $SAVE_PATH
+    --save_path $SAVE_PATH \
+    --D_h 512 \
+    --dropout_prob 0.41813713193324464 \
+    --alpha 0.29939853249854825 \
+    --lambda_orthogonality 0.00016402662815016467 \
+    --lr 0.0018359455575357815 \
+    --M 13 \
+    --t 13
 
 # Cleanup and Closeout
 echo "Deactivating virtual environment..."
