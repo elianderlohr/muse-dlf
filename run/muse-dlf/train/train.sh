39a518e5ecd3a37cff7dfa075a0b5d06730e23d9
elianderlohr <elias.anderlohr@t-online.de>
2024-03-22 23:00:59 +0100

    Add force_recalculate_frameaxis flag to training script

--- run/muse-dlf/train/train.sh
+++ run/muse-dlf/train/train.sh
@@ -99,7 +99,8 @@ accelerate launch --multi_gpu --num_processes 2 --num_machines 1 --mixed_precisi
     --lambda_orthogonality 0.00016402662815016467 \
     --lr 0.0018359455575357815 \
     --M 13 \
-    --t 13 
+    --t 13 \
+    --force_recalculate_frameaxis True
 
 # Cleanup and Closeout
 echo "Deactivating virtual environment..."
