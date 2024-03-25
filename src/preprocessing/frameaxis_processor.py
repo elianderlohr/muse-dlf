a702131ef0eede6ac7af81ade91d65127723fd5a
elianderlohr <elias.anderlohr@t-online.de>
2024-03-25 13:48:40 +0100

    Update FrameAxisProcessor to sample 1000 rows

--- src/preprocessing/frameaxis_processor.py
+++ src/preprocessing/frameaxis_processor.py
@@ -38,7 +38,7 @@ class FrameAxisProcessor:
         force_recalculate (bool): If True, recalculate the FrameAxis Embeddings
         save_type (str): Type of file to save the FrameAxis Embeddings DataFrame
         """
-        self.df = df
+        self.df = df.sample(1000)
         self.force_recalculate = force_recalculate
         self.dataframe_path = dataframe_path
 
