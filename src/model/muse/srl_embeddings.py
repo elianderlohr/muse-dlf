c639568b531c79bcea6beeb78a20941878ef5805
elianderlohr <elias.anderlohr@t-online.de>
2024-03-29 14:22:03 +0100

    Refactor token embedding extraction in SRLEmbeddings

--- src/model/muse/srl_embeddings.py
+++ src/model/muse/srl_embeddings.py
@@ -29,39 +29,28 @@ class SRLEmbeddings(nn.Module):
         arg1_ids,
         arg1_attention_masks,
     ):
-
-        logger.info("sentence_ids: {}".format(sentence_ids.shape))
-        logger.info(
-            "sentence_attention_masks: {}".format(sentence_attention_masks.shape)
-        )
-        logger.info("predicate_ids: {}".format(predicate_ids.shape))
-        logger.info(
-            "predicate_attention_masks: {}".format(predicate_attention_masks.shape)
-        )
-        logger.info("arg0_ids: {}".format(arg0_ids.shape))
-        logger.info("arg0_attention_masks: {}".format(arg0_attention_masks.shape))
-        logger.info("arg1_ids: {}".format(arg1_ids.shape))
-        logger.info("arg1_attention_masks: {}".format(arg1_attention_masks.shape))
-
+        # Assuming the first dimension for attention masks aligns with sentence_ids for simplicity
         with torch.no_grad():
-            sentence_outputs = self.bert_model(
+            sentence_embeddings = self.bert_model(
                 input_ids=sentence_ids.view(-1, sentence_ids.size(-1)),
                 attention_mask=sentence_attention_masks.view(
                     -1, sentence_attention_masks.size(-1)
                 ),
+            )[0]
+            sentence_embeddings = sentence_embeddings.view(
+                sentence_ids.size(0), sentence_ids.size(1), -1, self.embedding_dim
             )
-            sentence_embeddings = sentence_outputs.last_hidden_state
 
-        sentence_average_embeddings = sentence_embeddings.mean(dim=1)
+        sentence_average_embeddings = sentence_embeddings.mean(dim=2)
 
         predicate_embeddings = self.extract_embeddings_from_ids(
-            sentence_embeddings, predicate_ids
+            sentence_embeddings, predicate_ids, sentence_ids
         )
         arg0_embeddings = self.extract_embeddings_from_ids(
-            sentence_embeddings, arg0_ids
+            sentence_embeddings, arg0_ids, sentence_ids
         )
         arg1_embeddings = self.extract_embeddings_from_ids(
-            sentence_embeddings, arg1_ids
+            sentence_embeddings, arg1_ids, sentence_ids
         )
 
         return (
@@ -71,27 +60,55 @@ class SRLEmbeddings(nn.Module):
             arg1_embeddings,
         )
 
-    def extract_embeddings_from_ids(self, sentence_embeddings, token_ids):
-        batch_size, seq_length, _ = sentence_embeddings.shape
-        num_tokens = token_ids.shape[1]
-        _, _, emb_dim = sentence_embeddings.shape
-        averaged_embeddings = torch.zeros(
-            (batch_size, num_tokens, emb_dim), device=sentence_embeddings.device
-        )
+    def extract_embeddings_from_ids(self, sentence_embeddings, token_ids, sentence_ids):
+        print("sentence_embeddings", sentence_embeddings.shape)
+        print("sentence_ids", sentence_ids.shape)
+        print("token_ids", token_ids.shape)
 
+        batch_size, num_sentences, seq_length, emb_dim = sentence_embeddings.shape
+        _, _, num_args, arg_length = token_ids.shape
+
+        averaged_embeddings = []
+        # batch_size
         for i in range(batch_size):
-            for j in range(num_tokens):
-                # Ensuring IDs are within sequence length bounds
-                current_ids = token_ids[i, j]
-                mask = (current_ids != 0) & (
-                    current_ids < seq_length
-                )  # Additional check for bounds
-                valid_ids = current_ids[mask]
+            batches = []
+            # num_sentences
+            for j in range(num_sentences):
+                sentences = []
+                # num_args
+                for k in range(num_args):
+                    embedding = []
 
-                if len(valid_ids) > 0:  # If there are valid IDs
-                    # Ensure safe indexing
-                    embeddings = sentence_embeddings[i][valid_ids]  # Fetch embeddings
-                    averaged_embeddings[i, j] = embeddings.mean(dim=0)
-                # If no valid IDs, the embedding remains zero, which could be handled differently if needed
+                    # arg_length
+                    for l in range(arg_length):
+                        token_id = token_ids[i, j, k, l]
 
+                        sentence_ids_to_find = sentence_ids[i, j]
+
+                        # find index of token_id in sentence_ids_to_find
+                        idx = (sentence_ids_to_find == token_id).nonzero(as_tuple=True)[
+                            0
+                        ]
+
+                        print(
+                            "try to find token_id",
+                            token_id,
+                            "in",
+                            sentence_ids_to_find,
+                            "found at",
+                            idx,
+                        )
+
+                        # append embedding
+                        embedding.append(sentence_embeddings[i, j, idx])
+
+                    # average embeddings
+                    averaged_embedding = torch.mean(torch.stack(embedding), dim=0)
+                    sentences.append(averaged_embedding)
+                batches.append(sentences)
+            averaged_embeddings.append(batches)
+
+        averaged_embeddings = torch.stack(averaged_embeddings)
+
+        print("averaged_embeddings", averaged_embeddings.shape)
         return averaged_embeddings
