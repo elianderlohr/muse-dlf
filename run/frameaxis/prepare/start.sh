57e0170403ec1c27f07340837a0db6713034614d
elianderlohr <elias.anderlohr@t-online.de>
2024-04-04 13:57:52 +0200

    Add sample size parameter to frameaxis.py

--- run/frameaxis/prepare/start.sh
+++ run/frameaxis/prepare/start.sh
@@ -53,7 +53,8 @@ python src/frameaxis.py \
     --path_antonym_pairs $PATH_ANTONYM_PAIRS \
     --model_path $MODEL_PATH \
     --output_path $OUTPUT_PATH \
-    --dim_names $DIM_NAMES
+    --dim_names $DIM_NAMES \
+    --sample_size 1000
 
 # Cleanup and Closeout
 echo "Deactivating virtual environment..."
