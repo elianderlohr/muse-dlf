944257de8dc680e230fbec01bac6094520e202e9
elianderlohr <elias.anderlohr@t-online.de>
2024-04-15 08:50:38 +0200

    Update data path in .gitignore and wandb agent commands in sweep scripts

--- run/muse-dlf/sweep/sweep-8.sh
+++ run/muse-dlf/sweep/sweep-8.sh
@@ -61,14 +61,14 @@ export DIM_NAMES="virtue,vice"
 # Training Script Execution
 echo "=================== Training Start ==================="
 
-CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=4 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=5 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=6 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=7 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
+CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=4 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=5 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=6 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=7 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
 
 # Wait for all background jobs to finish
 wait
--- run/muse-dlf/sweep/sweep.sh
+++ run/muse-dlf/sweep/sweep.sh
@@ -61,10 +61,10 @@ export DIM_NAMES="virtue,vice"
 # Training Script Execution
 echo "=================== Training Start ==================="
 
-CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
-CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/muse-dlf/ntea057s &
+CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
+CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/muse-dlf/q4j345wd &
 
 # Wait for all background jobs to finish
 wait
