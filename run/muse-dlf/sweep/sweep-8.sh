d9161b460bac80aef676e17006fedfb646f76050
elianderlohr <elias.anderlohr@t-online.de>
2024-04-24 09:03:27 +0200

    Update wandb agent commands in sweep scripts to use new model

--- run/muse-dlf/sweep/sweep-8.sh
+++ run/muse-dlf/sweep/sweep-8.sh
@@ -61,14 +61,14 @@ export DIM_NAMES="virtue,vice"
 # Training Script Execution
 echo "=================== Training Start ==================="
 
-CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=4 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=5 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=6 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
-CUDA_VISIBLE_DEVICES=7 python -m wandb agent --count 50 elianderlohr/muse-dlf/f1e54z7h &
+CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=4 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=5 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=6 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
+CUDA_VISIBLE_DEVICES=7 python -m wandb agent --count 50 elianderlohr/muse-dlf/46qi6l9n &
 
 # Wait for all background jobs to finish
 wait
