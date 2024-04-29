4ca107f3406077ea95bae7799b0ebf58ed576177
elianderlohr <elias.anderlohr@t-online.de>
2024-04-29 08:32:33 +0200

    Refactor MUSESupervised class to improve code readability and efficiency

--- src/model/muse/supervised_module.py
+++ src/model/muse/supervised_module.py
@@ -19,18 +19,22 @@ class MUSESupervised(nn.Module):
         self.D_w = D_w
         self.frameaxis_dim = frameaxis_dim
 
-        self.dropout_1 = nn.Dropout(dropout_prob)
-        self.dropout_2 = nn.Dropout(dropout_prob)
-
         wr_shape = (
             D_w + (frameaxis_dim if sentence_prediction_method == "custom" else 0)
         ) * num_sentences
 
-        self.Wr = nn.Linear(wr_shape, D_w * num_sentences)
-        self.Wt = nn.Linear(D_w * num_sentences, K * num_sentences)
-        self.relu = nn.ReLU()
-
         self.flatten = nn.Flatten(start_dim=1)
+
+        self.feed_forward_sentence = nn.Sequential(
+            nn.Linear(wr_shape, D_w * num_sentences),
+            nn.BatchNorm1d(D_w * num_sentences),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
+            nn.Linear(D_w * num_sentences, D_w * num_sentences),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
+            nn.Linear(D_w * num_sentences, num_frames * num_sentences),
+        )
 
         self.sentence_prediction_method = sentence_prediction_method
 
@@ -47,23 +51,29 @@ class MUSESupervised(nn.Module):
     ):
         batch_size, num_sentences, _, _ = args_mask.size()
 
-        # Initialize masks
+        # args masks
         args_sentence_mask = args_mask.any(dim=3)
+        # attention maks
+        sentence_mask = sentence_attention_mask.any(dim=2)
 
         # mean to dim 8, 32, ignore where mask is False
         d_p_sentence_masked = d_p * args_sentence_mask.unsqueeze(-1)
         d_p_sentence = d_p_sentence_masked.mean(dim=2)
-        d_p_mean = d_p_sentence.mean(dim=1)
+        d_p_masked = d_p_sentence * sentence_mask.unsqueeze(-1)
+        d_p_mean = d_p_masked.mean(dim=1)
 
         d_a0_sentence_masked = d_a0 * args_sentence_mask.unsqueeze(-1)
         d_a0_sentence = d_a0_sentence_masked.mean(dim=2)
-        d_a0_mean = d_a0_sentence.mean(dim=1)
+        d_a0_masked = d_a0_sentence * sentence_mask.unsqueeze(-1)
+        d_a0_mean = d_a0_masked.mean(dim=1)
 
         d_a1_sentence_masked = d_a1 * args_sentence_mask.unsqueeze(-1)
         d_a1_sentence = d_a1_sentence_masked.mean(dim=2)
-        d_a1_mean = d_a1_sentence.mean(dim=1)
+        d_a1_masked = d_a1_sentence * sentence_mask.unsqueeze(-1)
+        d_a1_mean = d_a1_masked.mean(dim=1)
 
-        d_fx_mean = d_fx.mean(dim=1)
+        d_fx_masked = d_fx * sentence_mask.unsqueeze(-1)
+        d_fx_mean = d_fx_masked.mean(dim=1)
 
         # Combine and normalize the final descriptor
         y_hat_u = (d_p_mean + d_a0_mean + d_a1_mean + d_fx_mean) / 4
@@ -73,16 +83,9 @@ class MUSESupervised(nn.Module):
 
         ws_flattened = self.flatten(vs)
 
-        ws = self.dropout_1(ws_flattened)
-        ws = self.relu(self.Wr(ws))
-
-        ws = self.dropout_2(ws)
-        ws = self.Wt(ws)
+        ws = self.feed_forward_sentence(ws_flattened)
 
         ws_unflatten = ws.view(batch_size, num_sentences, -1)
-
-        # attention maks
-        sentence_mask = sentence_attention_mask.any(dim=2)
 
         ws_unflatten = ws_unflatten * sentence_mask.unsqueeze(-1)
         y_hat_s = ws_unflatten.sum(dim=1) / sentence_mask.sum(dim=1).unsqueeze(-1)
