6b49aed5a0cb2b51846b0e98782776ab01730223
elianderlohr <elias.anderlohr@t-online.de>
2024-04-29 13:09:59 +0200

    Refactor MUSESupervised class to calculate mean values for d_p, d_a0, d_a1, and d_fx and improve code readability

--- src/model/muse/supervised_module.py
+++ src/model/muse/supervised_module.py
@@ -55,51 +55,13 @@ class MUSESupervised(nn.Module):
         sentence_attention_mask,
         args_mask,
     ):
-        batch_size, num_sentences, _, _ = args_mask.size()
-
-        # args masks
-        args_sentence_mask = args_mask.any(dim=3)
-        sentence_mask = sentence_attention_mask.any(dim=2)
-
-        # mean to dim 8, 32, ignore where mask is False
-        d_p_sentence_masked = d_p * args_sentence_mask.unsqueeze(-1)
-        d_p_sentence_masked_sum = d_p_sentence_masked.sum(dim=2)
-        args_sentence_mask_count = args_sentence_mask.sum(dim=2).unsqueeze(-1)
-        d_p_sentence = torch.where(
-            args_sentence_mask_count > 0,
-            d_p_sentence_masked_sum / args_sentence_mask_count,
-            torch.zeros_like(d_p_sentence_masked_sum),
-        )
-        d_p_masked = d_p_sentence * sentence_mask.unsqueeze(-1)
-        d_p_mean = d_p_masked.sum(dim=1) / sentence_mask.sum(dim=1).unsqueeze(-1)
-
-        d_a0_sentence_masked = d_a0 * args_sentence_mask.unsqueeze(-1)
-        d_a0_sentence_masked_sum = d_a0_sentence_masked.sum(dim=2)
-        args_sentence_mask_count_a0 = args_sentence_mask.sum(dim=2).unsqueeze(-1)
-        d_a0_sentence = torch.where(
-            args_sentence_mask_count_a0 > 0,
-            d_a0_sentence_masked_sum / args_sentence_mask_count_a0,
-            torch.zeros_like(d_a0_sentence_masked_sum),
-        )
-        d_a0_masked = d_a0_sentence * sentence_mask.unsqueeze(-1)
-        d_a0_mean = d_a0_masked.sum(dim=1) / sentence_mask.sum(dim=1).unsqueeze(-1)
-
-        d_a1_sentence_masked = d_a1 * args_sentence_mask.unsqueeze(-1)
-        d_a1_sentence_masked_sum = d_a1_sentence_masked.sum(dim=2)
-        args_sentence_mask_count_a1 = args_sentence_mask.sum(dim=2).unsqueeze(-1)
-        d_a1_sentence = torch.where(
-            args_sentence_mask_count_a1 > 0,
-            d_a1_sentence_masked_sum / args_sentence_mask_count_a1,
-            torch.zeros_like(d_a1_sentence_masked_sum),
-        )
-        d_a1_masked = d_a1_sentence * sentence_mask.unsqueeze(-1)
-        d_a1_mean = d_a1_masked.sum(dim=1) / sentence_mask.sum(dim=1).unsqueeze(-1)
-
-        d_fx_masked = d_fx * sentence_mask.unsqueeze(-1)
-        d_fx_mean = d_fx_masked.sum(dim=1) / sentence_mask.sum(dim=1).unsqueeze(-1)
+        d_p_mean = torch.mean(d_p, dim=1)
+        d_a0_mean = torch.mean(d_a0, dim=1)
+        d_a1_mean = torch.mean(d_a1, dim=1)
 
         # Combine and normalize the final descriptor
-        y_hat_u = (d_p_mean + d_a0_mean + d_a1_mean + d_fx_mean) / 4
+        w_u = (d_p_mean + d_a0_mean + d_a1_mean + d_fx) / 4
+        y_hat_u = w_u.sum(dim=1)
 
         if self.sentence_prediction_method == "custom":
             vs = torch.cat([vs, frameaxis_data], dim=-1)
