2a9cd3afb4d29e0121b1c19aa7807a0ffda545de
elianderlohr <elias.anderlohr@t-online.de>
2024-04-29 15:22:57 +0200

    Update GPU resource allocation in mlm.sh script

--- run/finetune-roberta/mlm.sh
+++ run/finetune-roberta/mlm.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=roberta-base-finetune
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elias.anderlohr@gmail.com
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 
 echo "===================== Job Details ====================="
 # Activate the virtual environment
@@ -62,7 +62,17 @@ echo "=================== Training Start ==================="
 # accelerate config --config_file run/accelerate_config.yaml
 
 echo "Launching training script with Accelerate..."
-accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precision fp16 --config_file run/accelerate_config.yaml src/training/mlm.py --wb_api_key $WANDB_API_KEY --data_path $DATA_PATH --output_path $OUTPUT_PATH --batch_size 32 --epochs 1000 --patience 15
+accelerate launch --multi_gpu \
+    --num_processes 4 \
+    --num_machines 1 \
+    --mixed_precision fp16 \
+    --config_file run/accelerate_config.yaml src/training/mlm.py \
+    --wb_api_key $WANDB_API_KEY \
+    --data_path $DATA_PATH \
+    --output_path $OUTPUT_PATH \
+    --batch_size 32 \
+    --epochs 1000 \
+    --patience 15
 
 # Cleanup and Closeout
 echo "Deactivating virtual environment..."
