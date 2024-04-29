Fix shape mismatch in MUSE class unsupervised_loss calculation

src/model/muse/muse.py
@@ -260,7 +260,9 @@ class MUSE(nn.Module):
 
         # Take average by summing the valid losses and dividing by num sentences so that padded sentences are also taken in equation
         unsupervised_loss = unsupervised_losses[valid_losses].sum() / (
-            sentence_embeddings.shape[0] * sentence_embeddings.shape[1]
+            sentence_embeddings.shape[0]
+            * sentence_embeddings.shape[1]
+            * sentence_embeddings.shape[2]
         )
 
         return unsupervised_loss, span_pred, sentence_pred, combined_pred