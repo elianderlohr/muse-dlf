b7d69a93aaf782ae14c5ad61d756eb00dd65949a
elianderlohr <elias.anderlohr@t-online.de>
2024-04-29 14:53:27 +0200

    Update virtual environment activation paths in mlm.sh and start.sh scripts

--- run/frameaxis/prepare/start.sh
+++ run/frameaxis/prepare/start.sh
@@ -11,7 +11,7 @@
 echo "===================== Job Details ====================="
 # Activate the virtual environment
 echo "Activating virtual environment..."
-source run/muse-dlf/muse-dlf/bin/activate
+source run/venv/bin/activate
 
 # Environment Setup
 echo "Setting up environment..."
