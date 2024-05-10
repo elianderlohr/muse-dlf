e8a7b1f32ab336475c2c109eab17818a899a98dd
elianderlohr <elias.anderlohr@t-online.de>
2024-05-10 13:45:12 +0200

    chore: Update epochs to 20 in mlm-sweep.sh script

--- run/finetune-roberta/mlm-sweep.sh
+++ run/finetune-roberta/mlm-sweep.sh
@@ -60,7 +60,7 @@ CUDA_VISIBLE_DEVICES=0 python src/mlm-sweep.py \
     --model_name roberta-base \
     --project_name "roberta-base-finetune" \
     --batch_size 8 \
-    --epochs 10 \
+    --epochs 20 \
     --patience 15 &
 CUDA_VISIBLE_DEVICES=1 python src/mlm-sweep.py \
     --wb_api_key $WANDB_API_KEY \
@@ -69,7 +69,7 @@ CUDA_VISIBLE_DEVICES=1 python src/mlm-sweep.py \
     --model_name roberta-base \
     --project_name "roberta-base-finetune" \
     --batch_size 16 \
-    --epochs 10 \
+    --epochs 20 \
     --patience 15 &
 CUDA_VISIBLE_DEVICES=2 python src/mlm-sweep.py \
     --wb_api_key $WANDB_API_KEY \
@@ -78,7 +78,7 @@ CUDA_VISIBLE_DEVICES=2 python src/mlm-sweep.py \
     --model_name roberta-base \
     --project_name "roberta-base-finetune" \
     --batch_size 24 \
-    --epochs 10 \
+    --epochs 20 \
     --patience 15 &
 CUDA_VISIBLE_DEVICES=3 python src/mlm-sweep.py \
     --wb_api_key $WANDB_API_KEY \
@@ -87,7 +87,7 @@ CUDA_VISIBLE_DEVICES=3 python src/mlm-sweep.py \
     --model_name roberta-base \
     --project_name "roberta-base-finetune" \
     --batch_size 32 \
-    --epochs 10 \
+    --epochs 20 \
     --patience 15 &
 
 # Wait for all processes to complete
