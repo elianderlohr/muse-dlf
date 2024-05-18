b7cdc21cf8bab9ae410b2a8d00318cbf3de65e6a
elianderlohr <elias.anderlohr@t-online.de>
2024-05-18 15:57:41 +0200

    chore: Update data and output paths

--- run/frameaxis/prepare/start.sh
+++ run/frameaxis/prepare/start.sh
@@ -25,11 +25,11 @@ python -m pip list
 
 # Data and Output Configuration
 echo "Configuring paths..."
-DATA_PATH="data/mfc/immigration_labeled_preprocessed_unsplitted.json"
+DATA_PATH="data/mfc/immigration_labeled_preprocessed.json"
 PATH_ANTONYM_PAIRS="data/axis/mft.json"
 DIM_NAMES="virtue,vice"
 MODEL_PATH="models/roberta-base-finetune/roberta-base-finetune-2024-05-16_05-34-31-73594/checkpoint-32060"
-OUTPUT_PATH="data/frameaxis/mfc/frameaxis_mft_document.pkl"
+OUTPUT_PATH="data/frameaxis/mfc/frameaxis_mft.pkl"
 PATH_MICROFRAME="data/frameaxis/mfc/frameaxis_mft_microframes.pkl"
 
 echo "Data path: $DATA_PATH"
