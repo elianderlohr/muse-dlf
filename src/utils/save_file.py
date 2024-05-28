c67dcb52ca95c4a0bddf4fb12f92fcb757127e58
elianderlohr <elias.anderlohr@t-online.de>
2024-05-28 17:02:38 +0200

    chore: Save RoBERTa model locally and update artifact path

--- src/utils/save_file.py
+++ src/utils/save_file.py
@@ -7,9 +7,13 @@ import wandb
 model_path = "models/roberta-base-finetune/roberta-base-finetune-2024-05-20_08-02-29-65707/checkpoint-16482"
 model = RobertaForSequenceClassification.from_pretrained(model_path)
 
+# Save the model locally
+save_path = model_path + "/roberta_model.pth"
+torch.save(model.state_dict(), save_path)
+
 # Initialize wandb and save the model as an artifact for version control
 run = wandb.init(project="roberta-base-finetune")
 artifact = wandb.Artifact("roberta_model", type="model")
-artifact.add_file(model_path)
+artifact.add_file(save_path)
 run.log_artifact(artifact)
 run.finish()
