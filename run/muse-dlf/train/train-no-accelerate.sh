e450c09425edcff9fc38238edbcb78f46494a142
elianderlohr <elias.anderlohr@t-online.de>
2024-05-31 10:28:03 +0200

    chore: Update memory allocation in training script

--- run/muse-dlf/train/train-no-accelerate.sh
+++ run/muse-dlf/train/train-no-accelerate.sh
@@ -5,6 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --job-name=muse-dlf-train
 #SBATCH --gres=gpu:1
+#SBATCH --mem=32G
 
 echo "===================== Job Details ====================="
 # Activate the virtual environment
