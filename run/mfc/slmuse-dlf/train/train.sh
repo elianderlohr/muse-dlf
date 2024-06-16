0e55bfb313f943921f253bad4cc4471b638bac2c
elianderlohr <elias.anderlohr@t-online.de>
2024-06-16 09:11:14 +0200

    fix: increase train epochs

--- run/mfc/slmuse-dlf/train/train.sh
+++ run/mfc/slmuse-dlf/train/train.sh
@@ -89,7 +89,7 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --tags $TAGS \
     --wandb_api_key $WANDB_API_KEY \
     --path_data $DATA_PATH \
-    --epochs 10 \
+    --epochs 20 \
     --frameaxis_dim 10 \
     --name_tokenizer roberta-base \
     --path_name_bert_model models/roberta-base-finetune/roberta-base-finetune-2024-05-20_08-02-29-65707/checkpoint-16482 \
