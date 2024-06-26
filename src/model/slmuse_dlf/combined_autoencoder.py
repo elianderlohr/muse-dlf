fix: remove gumbel softmax hard

src/model/slmuse_dlf/combined_autoencoder.py
@@ -17,7 +17,6 @@ class CombinedAutoencoder(nn.Module):
         activation="relu",  # activation function (relu, gelu, leaky_relu, elu)
         use_batch_norm=True,  # whether to use batch normalization
         matmul_input="g",  # g or d (g = gumbel-softmax, d = softmax)
-        hard=False,  # whether to use hard gumbel softmax
         log=False,  # whether to use log gumbel softmax
         _debug=False,
     ):
@@ -31,7 +30,6 @@ class CombinedAutoencoder(nn.Module):
         self.num_layers = num_layers
         self.use_batch_norm = use_batch_norm
         self.matmul_input = matmul_input
-        self.hard = hard
         self.log = log
 
         # Initialize activation function
@@ -174,13 +172,9 @@ class CombinedAutoencoder(nn.Module):
             ):
                 self.logger.error("❌ NaNs detected in d or d is all 0 AFTER softmax")
 
-            g_p = self.custom_gumbel_softmax(d_p, tau=tau, hard=self.hard, log=self.log)
-            g_a0 = self.custom_gumbel_softmax(
-                d_a0, tau=tau, hard=self.hard, log=self.log
-            )
-            g_a1 = self.custom_gumbel_softmax(
-                d_a1, tau=tau, hard=self.hard, log=self.log
-            )
+            g_p = self.custom_gumbel_softmax(d_p, tau=tau, hard=False, log=self.log)
+            g_a0 = self.custom_gumbel_softmax(d_a0, tau=tau, hard=False, log=self.log)
+            g_a1 = self.custom_gumbel_softmax(d_a1, tau=tau, hard=False, log=self.log)
 
             # Check for NaNs in g
             if (
@@ -189,7 +183,7 @@ class CombinedAutoencoder(nn.Module):
                 or torch.isnan(g_a1).any()
             ):
                 self.logger.error(
-                    f"❌ NaNs detected in g AFTER gumbel-softmax, tau: {tau}, hard: {self.hard}, log: {self.log}"
+                    f"❌ NaNs detected in g AFTER gumbel-softmax, tau: {tau}, hard: {False}, log: {self.log}"
                 )
 
             if self.matmul_input == "d":

src/model/slmuse_dlf/frameaxis_autoencoder.py
@@ -21,7 +21,6 @@ class FrameAxisAutoencoder(nn.Module):
         use_batch_norm=True,  # whether to use batch normalization
         matmul_input="g",  # g or d (g = gumbel-softmax, d = softmax)
         concat_frameaxis=True,  # whether to concatenate frameaxis with sentence
-        hard=False,  # whether to use hard gumbel softmax
         log=False,  # whether to use log gumbel softmax
         _debug=False,
     ):
@@ -35,7 +34,6 @@ class FrameAxisAutoencoder(nn.Module):
         self.use_batch_norm = use_batch_norm
         self.matmul_input = matmul_input
         self.concat_frameaxis = concat_frameaxis
-        self.hard = hard
         self.log = log
 
         # Initialize activation function
@@ -138,7 +136,7 @@ class FrameAxisAutoencoder(nn.Module):
 
             d = torch.softmax(logits, dim=1)
 
-            g = self.custom_gumbel_softmax(d, tau=tau, hard=self.hard, log=self.log)
+            g = self.custom_gumbel_softmax(d, tau=tau, hard=False, log=self.log)
 
             if self.matmul_input == "d":
                 vhat = torch.matmul(d, self.F)

src/model/slmuse_dlf/unsupervised_module.py
@@ -25,7 +25,6 @@ class MUSEUnsupervised(nn.Module):
         activation="relu",  # activation function (relu, gelu, leaky_relu, elu)
         use_batch_norm=True,  # whether to use batch normalization
         matmul_input="g",  # g or d (g = gumbel-softmax, d = softmax)
-        gumbel_softmax_hard=False,  # whether to use hard gumbel softmax
         gumbel_softmax_log=False,  # whether to use log gumbel softmax
         _debug=False,
     ):
@@ -43,7 +42,6 @@ class MUSEUnsupervised(nn.Module):
             activation=activation,
             use_batch_norm=use_batch_norm,
             matmul_input=matmul_input,
-            hard=gumbel_softmax_hard,
             log=gumbel_softmax_log,
             _debug=_debug,
         )
@@ -75,15 +73,15 @@ class MUSEUnsupervised(nn.Module):
             outputs = self.combined_autoencoder(v_p, v_a0, v_a1, v_sentence, tau)
 
             # check if p g has nan values or 0 values
-            if torch.isnan(outputs["p"]["g"]).any() or (outputs["p"]["g"] == 0).all():
+            if torch.isnan(outputs["p"]["g"]).any() or (outputs["p"]["g"] == 0).any():
                 self.logger.debug(f"🚨 p g has nan values or 0 values")
 
             # check if a0 g has nan values or 0 values
-            if torch.isnan(outputs["a0"]["g"]).any() or (outputs["a0"]["g"] == 0).all():
+            if torch.isnan(outputs["a0"]["g"]).any() or (outputs["a0"]["g"] == 0).any():
                 self.logger.debug(f"🚨 a0 g has nan values or 0 values")
 
             # check if a1 g has nan values or 0 values
-            if torch.isnan(outputs["a1"]["g"]).any() or (outputs["a1"]["g"] == 0).all():
+            if torch.isnan(outputs["a1"]["g"]).any() or (outputs["a1"]["g"] == 0).any():
                 self.logger.debug(f"🚨 a1 g has nan values or 0 values")
 
             outputs_p = outputs["p"]