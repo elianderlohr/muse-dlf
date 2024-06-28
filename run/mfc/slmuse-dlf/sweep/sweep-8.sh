2fca0663fb1e6a3647f78a585329d2f8a3f9898e
elianderlohr <elias.anderlohr@t-online.de>
2024-06-28 10:50:43 +0200

    fix: sweep-4 and sweep-8

--- run/mfc/slmuse-dlf/sweep/sweep-8.sh
+++ run/mfc/slmuse-dlf/sweep/sweep-8.sh
@@ -60,14 +60,14 @@ export DIM_NAMES="virtue,vice"
 # Training Script Execution
 echo "=================== Training Start ==================="
 
-CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=4 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=5 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=6 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
-CUDA_VISIBLE_DEVICES=7 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/xjstyz6s &
+CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=4 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=5 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=6 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
+CUDA_VISIBLE_DEVICES=7 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/hv4234zs &
 
 # Wait for all background jobs to finish
 wait
