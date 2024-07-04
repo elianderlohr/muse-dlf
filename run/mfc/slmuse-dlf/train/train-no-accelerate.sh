486094f7959d34b807351a2d8e2c8392a55dfa58
elianderlohr <elias.anderlohr@t-online.de>
2024-07-04 08:00:44 +0200

    refactor: Update batch_size to 32 in train-no-accelerate.sh

--- run/mfc/slmuse-dlf/train/train-no-accelerate.sh
+++ run/mfc/slmuse-dlf/train/train-no-accelerate.sh
@@ -102,7 +102,7 @@ python src/start_train.py \
     --lr 0.0005 \
     --M 8 \
     --t 8 \
-    --batch_size 8 \
+    --batch_size 32 \
     --num_sentences 32 \
     --max_sentence_length 64 \
     --max_args_per_sentence 10 \
