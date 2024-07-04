09b1f7f2b59820e742fbe9c906f2195bdd3f60ea
elianderlohr <elias.anderlohr@t-online.de>
2024-07-04 14:44:53 +0200

    refactor: Update CUDA_VISIBLE_DEVICES in sweep scripts

--- run/mfc/slmuse-dlf/sweep/sweep-4.sh
+++ run/mfc/slmuse-dlf/sweep/sweep-4.sh
@@ -61,10 +61,10 @@ export MODEL_TYPE="slmuse-dlf"
 # Training Script Execution
 echo "=================== Training Start ==================="
 
-CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/cyyef6fu &
-CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/cyyef6fu &
-CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/cyyef6fu &
-CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/cyyef6fu &
+CUDA_VISIBLE_DEVICES=0 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/d2na6i2u &
+CUDA_VISIBLE_DEVICES=1 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/d2na6i2u &
+CUDA_VISIBLE_DEVICES=2 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/d2na6i2u &
+CUDA_VISIBLE_DEVICES=3 python -m wandb agent --count 50 elianderlohr/slmuse-dlf/d2na6i2u &
 
 # Wait for all background jobs to finish
 wait
