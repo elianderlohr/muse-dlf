9a20e4ccf7bc130694d5c9ecdf9c1a23927c536e
elianderlohr <elias.anderlohr@t-online.de>
2024-07-08 09:29:52 +0200

    chore: Update submit_jobs.sh with additional SLURM options

--- run/mfc/slmuse-dlf/hp/submit_jobs.sh
+++ run/mfc/slmuse-dlf/hp/submit_jobs.sh
@@ -14,6 +14,8 @@ do
 #SBATCH --job-name=mfc-slmuse-dlf-batch-${split_id}
 #SBATCH --gres=gpu:4
 #SBATCH --mem=48G
+#SBATCH --time=24:00:00
+#SBATCH --partition=single
 
 echo "===================== Job Details ====================="
 echo "Job settings at start:"
