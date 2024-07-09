bdfd6835b025f799dded58b0a4521d486110a1a2
elianderlohr <elias.anderlohr@t-online.de>
2024-07-09 09:52:56 +0200

    refactor: Update output file name in generate_combinations.sh

--- run/mfc/slmuse-dlf/hp/generate_combinations.sh
+++ run/mfc/slmuse-dlf/hp/generate_combinations.sh
@@ -5,7 +5,7 @@ dropout_probs=(0.1 0.2 0.3 0.4 0.5)
 supervised_activations=("gelu" "relu")
 
 # Generate all combinations and write them to a file
-output_file="combinations.txt"
+output_file="new_combinations.txt"
 > $output_file
 
 for pooling in "${srl_embeddings_pooling[@]}"; do
--- run/mfc/slmuse-dlf/hp/split_combinations.sh
+++ run/mfc/slmuse-dlf/hp/split_combinations.sh
@@ -4,4 +4,4 @@
 num_splits=4
 
 # Split the combinations file
-split -l $(( $(wc -l < combinations.txt) / num_splits )) combinations.txt new_combinations_
+split -l $(( $(wc -l < new_combinations.txt) / num_splits )) new_combinations.txt new_combinations_
