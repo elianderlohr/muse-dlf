541866ed86b1b6aff3454536f7d488ae42040af5
elianderlohr <elias.anderlohr@t-online.de>
2024-07-11 11:04:47 +0200

    refactor: Update batch size to 8 in train.sh

--- run/mfc/slmuse-dlf/train/train.sh
+++ run/mfc/slmuse-dlf/train/train.sh
@@ -146,7 +146,7 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --seed 42 \
     --mixed_precision fp16 \
     --accumulation_steps 1 \
-    --alternative_supervised default \
+    --alternative_supervised alt2 \
     $DEBUG
 
 # Cleanup and Closeout
