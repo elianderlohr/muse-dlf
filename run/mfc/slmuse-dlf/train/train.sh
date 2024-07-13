fedb2e7bb9ca71717a9ebe5be9edd259abe420b0
elianderlohr <elias.anderlohr@t-online.de>
2024-07-13 09:43:21 +0200

    refactor: Update alternative_supervised parameter value to alt4

--- run/mfc/slmuse-dlf/train/train.sh
+++ run/mfc/slmuse-dlf/train/train.sh
@@ -183,7 +183,7 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --seed 42 \
     --mixed_precision fp16 \
     --accumulation_steps 1 \
-    --alternative_supervised alt5 \
+    --alternative_supervised alt4 \
     $DEBUG
 
 echo "______________________________________________________"
