8ab552755748ea206d05da9d18f1a5d0e219739a
elianderlohr <elias.anderlohr@t-online.de>
2024-07-16 20:28:09 +0200

    refactor: Update sentence_pooling and hidden_state parameters in train.sh

--- run/semeval/muse-dlf/train/train.sh
+++ run/semeval/muse-dlf/train/train.sh
@@ -177,8 +177,8 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --supervised_activation gelu \
     --adamw_weight_decay 0.0001 \
     --optimizer adamw \
-    --sentence_pooling mean \
-    --hidden_state last \
+    --sentence_pooling cls \
+    --hidden_state second_to_last \
     --tau_decay 0.0005 \
     --tau_min 0.5 \
     --seed 42 \
