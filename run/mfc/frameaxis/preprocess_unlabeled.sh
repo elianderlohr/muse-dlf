8412da6af88918f78b7c13a7cac8cc496e89e50d
elianderlohr <elias.anderlohr@t-online.de>
2024-07-18 08:43:51 +0200

    fix: add processing scripts for unlabeled mfc

---
+++ run/mfc/frameaxis/preprocess_unlabeled.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# SLURM Directives
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --job-name=mfc-frameaxis-1
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+
+echo "===================== Job Details ====================="
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source run/venv/bin/activate
+
+# Environment Setup
+echo "Setting up environment..."
+# Verify Python version
+echo "Python version:"
+python --version
+echo "pip version:"
+python -m pip --version
+
+# Display installed package versions for verification
+echo "Installed package versions:"
+python -m pip list
+
+# Data and Output Configuration
+echo "Configuring paths..."
+SAVE_PATH="data/frameaxis/mfc/unlabeled/"
+DATA_PATH="data/mfc/immigration_unlabeled_preprocessed.json"
+PATH_ANTONYM_PAIRS="data/axis/mft.json"
+DIM_NAMES="virtue,vice"
+ROBERTA_MODEL_PATH="models/roberta-base-finetune/roberta-base-finetune-2024-05-20_08-02-29-65707/checkpoint-16482"
+PATH_MICROFRAMES="data/frameaxis/mfc/frameaxis_mft_microframes.pkl"
+
+
+# GPU Setup and Verification
+echo "GPU status:"
+nvidia-smi
+
+# Training Script Execution
+echo "=================== Training Start ==================="
+# echo "Setting up Accelerate configuration..."
+
+echo "Running frameaxis.py..."
+python src/start_frameaxis.py \
+    --save_path $SAVE_PATH \
+    --data_path $DATA_PATH \
+    --path_antonym_pairs $PATH_ANTONYM_PAIRS \
+    --dim_names $DIM_NAMES \
+    --roberta_model_path $ROBERTA_MODEL_PATH \
+    --path_microframes $PATH_MICROFRAMES \
+    --word_blacklist "immigrant" "immigrants" "immigration" "illegal" "illegally" "illegals" "legally" "legalize" "legal"
+
+# Cleanup and Closeout
+echo "Deactivating virtual environment..."
+deactivate
+echo "==================== Job Complete ===================="
---
+++ run/mfc/srl/preprocess_unlabeled.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# SLURM Directives
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --job-name=mfc-srl-1
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+
+echo "===================== Job Details ====================="
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source run/srl/srl_venv/bin/activate
+
+# Environment Setup
+echo "Setting up environment..."
+# Verify Python version
+echo "Python version:"
+python --version
+echo "pip version:"
+python -m pip --version
+
+# Display installed package versions for verification
+echo "Installed package versions:"
+python -m pip list
+
+# Data and Output Configuration
+echo "Configuring paths..."
+DATA_PATH="data/mfc/immigration_unlabeled_preprocessed.json"
+OUTPUT_PATH="data/srls/mfc/mfc_unlabeled.pkl"
+
+echo "Data path: $DATA_PATH"
+echo "Output path: $OUTPUT_PATH"
+
+# GPU Setup and Verification
+echo "GPU status:"
+nvidia-smi
+
+export CUDA_VISIBLE_DEVICES=0
+
+# Training Script Execution
+echo "=================== Training Start ==================="
+# echo "Setting up Accelerate configuration..."
+
+echo "Running srls.py..."
+python src/start_srls.py \
+    --data_path $DATA_PATH \
+    --output_path $OUTPUT_PATH 
+
+# Cleanup and Closeout
+echo "Deactivating virtual environment..."
+deactivate
+echo "==================== Job Complete ===================="
