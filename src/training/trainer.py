83b815a6830f1e883dede16c7251fb89f46c8a35
elianderlohr <elias.anderlohr@t-online.de>
2024-07-24 22:00:49 +0200

    Update batch size to 8 for improved training performance

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1382,7 +1382,7 @@ class Trainer:
                 early_stopping["stopping_code"] = 104
                 break
 
-            if epoch > 1 and metrics["accuracy"] < 0.3:
+            if epoch > 2 and metrics["accuracy"] < 0.2:
                 logger.info("Accuracy is below 0.3. Stopping training.")
                 early_stopping["early_stopped"] = True
                 early_stopping["stopping_code"] = 102
@@ -1392,14 +1392,14 @@ class Trainer:
                 )
                 break
 
-            if epoch > 2 and metrics["accuracy"] < 0.4:
-                logger.info("Accuracy is below 0.4. Stopping training.")
-                early_stopping["early_stopped"] = True
-                early_stopping["stopping_code"] = 102
-                self._log_alert(
-                    title="Accuracy is below 0.4.",
-                    text="The model never surpassed 0.4 accuracy.",
-                )
-                break
+            # if epoch > 3 and metrics["accuracy"] < 0.3:
+            #    logger.info("Accuracy is below 0.4. Stopping training.")
+            #    early_stopping["early_stopped"] = True
+            #    early_stopping["stopping_code"] = 102
+            #    self._log_alert(
+            #        title="Accuracy is below 0.4.",
+            #        text="The model never surpassed 0.4 accuracy.",
+            #    )
+            #    break
 
         return early_stopping
