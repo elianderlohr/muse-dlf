0f7f6108f629e34f35e098a54456e96e94bdd095
elianderlohr <elias.anderlohr@t-online.de>
2024-07-29 08:36:08 +0200

    chore: Update partition to single for improved job submission

--- run/mfc/slmuse-dlf/hp/submit_jobs.sh
+++ run/mfc/slmuse-dlf/hp/submit_jobs.sh
@@ -15,7 +15,7 @@ do
 #SBATCH --gres=gpu:4
 #SBATCH --mem=48G
 #SBATCH --time=48:00:00
-#SBATCH --partition=simple
+#SBATCH --partition=single
 
 echo "===================== Job Details ====================="
 echo "Job settings at start:"
