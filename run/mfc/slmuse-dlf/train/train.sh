f020b1a97a57897617fc5874704abe2bd0475bc8
elianderlohr <elias.anderlohr@t-online.de>
2024-08-03 13:55:34 +0200

    refactor: Update learning rate and batch size in train.sh

--- run/mfc/slmuse-dlf/train/train.sh
+++ run/mfc/slmuse-dlf/train/train.sh
@@ -170,10 +170,10 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --dropout_prob 0.3 \
     --alpha 0.5 \
     --lambda_orthogonality 1e-3 \
-    --lr 0.0001 \
+    --lr 0.0005 \
     --M 8 \
     --t 8 \
-    --batch_size 32 \
+    --batch_size 8 \
     --num_sentences 24 \
     --max_sentence_length 64 \
     --max_args_per_sentence 10 \
@@ -193,7 +193,7 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --supervised_num_layers 2 \
     --supervised_activation gelu \
     --optimizer adamw \
-    --adamw_weight_decay 0.0001 \
+    --adamw_weight_decay 5e-7 \
     --ams_grad_options True \
     --sentence_pooling mean \
     --hidden_state second_to_last \
@@ -202,7 +202,7 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --seed 42 \
     --mixed_precision fp16 \
     --accumulation_steps 1 \
-    --alternative_supervised alt8 \
+    --alternative_supervised alt9 \
     --clip_value 1 \
     --focal_loss_gamma 2 \
     $DEBUG
