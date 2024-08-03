b3287e885fce86be68f80f012b2cd94235920ac8
elianderlohr <elias.anderlohr@t-online.de>
2024-08-03 22:06:23 +0200

    refactor: Update alternative supervised module to alt8 in train.sh

--- run/mfc/slmuse-dlf/train/train.sh
+++ run/mfc/slmuse-dlf/train/train.sh
@@ -202,7 +202,7 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --seed 42 \
     --mixed_precision fp16 \
     --accumulation_steps 1 \
-    --alternative_supervised alt9 \
+    --alternative_supervised alt8 \
     --clip_value 1 \
     --focal_loss_gamma 2 \
     $DEBUG
