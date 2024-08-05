90dc9bd4b7e2e3964bbfc54a2126d9cfb5797320
elianderlohr <elias.anderlohr@t-online.de>
2024-08-05 21:16:11 +0200

    refactor: Update num_sentences and max_sentence_length parameters in train.sh

--- run/semeval/muse-dlf/train/train.sh
+++ run/semeval/muse-dlf/train/train.sh
@@ -179,10 +179,10 @@ accelerate launch --multi_gpu --num_processes 4 --num_machines 1 --mixed_precisi
     --M 8 \
     --t 8 \
     --batch_size 8 \
-    --num_sentences 48 \
-    --max_sentence_length 40 \
-    --max_args_per_sentence 21 \
-    --max_arg_length 21 \
+    --num_sentences 32 \
+    --max_sentence_length 64 \
+    --max_args_per_sentence 10 \
+    --max_arg_length 10 \
     --muse_unsupervised_num_layers 2 \
     --muse_unsupervised_activation gelu \
     --muse_unsupervised_use_batch_norm True \
