6227f4120064ce626d119e9e8824c5225174d83d
elianderlohr <elias.anderlohr@t-online.de>
2024-08-09 22:14:25 +0200

    refactor: Update max value of cheat_alpha to 3 in start_train.py

--- src/start_train.py
+++ src/start_train.py
@@ -997,8 +997,8 @@ def main():
             # cheat alpha
             cheat_alpha = torch.tensor(list(class_freq_dict.values()))
             cheat_alpha = cheat_alpha * 100
-            # clamp max to 8
-            cheat_alpha = torch.clamp(cheat_alpha, max=4)
+            # clamp max to 3
+            cheat_alpha = torch.clamp(cheat_alpha, max=3)
             cheat_alpha = cheat_alpha.to(accelerator.device)
 
             loss_function = FocalLoss(
